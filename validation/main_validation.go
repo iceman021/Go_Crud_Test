c90ebc259b925bd105d50c22399ad47ff7080a17
Dusan <45889538+iceman021@users.noreply.github.com>
2022-03-27 19:54:45 +0200

    Validation file

---
+++ validation/main_validation.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-playground/validator"
+)
+
+type Author struct {
+	ID      int    `validate:"gte=1,lte=5"`
+	Name    string `json:"name" validate:"alpha"`
+	Surname string `json:"surname" validate:"alpha"`
+}
+
+type Book struct {
+	ID              int       `validate:"gte=1,lte=5"`
+	Title           string    `json:"title" validate:"alpha"`
+	Description     string    `json:"description" validate:"alpha"`
+	PublicationDate string 	  `validate:"datetime"`
+}
+
+/* User contains user information
+type Author struct {
+	FirstName   string `json:"fname" validate:"alpha"`
+	LastName    string `json:"lname" validate:"alpha"`
+	Age         uint8  `validate:"gte=20,lte=65"`
+	Email       string `json:"e-mail" validate:"required,email"`
+	JoiningDate string `validate:"datetime"`
+}*/
+
+// use single instance of Validate, it caches struct info
+var validate *validator.Validate
+
+func main() {
+
+	validate = validator.New()
+
+	book := &Book{
+		ID:   7,
+		Title:    "Test",
+		Description:  "markovic",
+		PublicationDate: "OneOne",
+	}
+
+	author := &Author{
+		ID:   6,
+		Name:    "Mark",
+		Surname:  "twain",
+	}
+
+
+	err := validate.Struct(author)
+	if err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			fmt.Println(err)
+			return
+		}
+
+		err := validate.Struct(book)
+	if err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			fmt.Println(err)
+			return
+		}
+	
+
+		fmt.Println("----- Listing of tag fields with error ------")
+
+		for _, err := range err.(validator.ValidationErrors) {
+			fmt.Println(err.StructField())
+			fmt.Println(err.ActualTag())
+			fmt.Println(err.Kind())
+			fmt.Println(err.Value())
+			fmt.Println(err.Param())
+			fmt.Println("---------------")
+		}
+		return
+	}
+}}
